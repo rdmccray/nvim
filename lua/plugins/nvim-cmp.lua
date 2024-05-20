708ba106ab6e35d689844a5899f28801e61abe3f
rdmccray <dennis@themccrays.net>
2024-05-20 17:32:49 -0500

    added lspkind to cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -12,6 +12,8 @@ return {
     },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
         -- Global setup.
         cmp.setup({
             snippet = {
@@ -24,18 +26,30 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-k>"] = cmp.mapping.select_next_item(),
+                ["<C-j>"] = cmp.mapping.select_prev_item(),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
                 { name = "buffer" },
+                { name = "path" },
             }),
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = 'symbol', -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                                    -- can also be a function to dynamically calculate max width such as 
+                                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                })
+            }
         })
         -- `/` cmdline setup.
         cmp.setup.cmdline("/", {
