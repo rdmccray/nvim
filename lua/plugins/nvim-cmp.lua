99fcacdda2a14e565a72d26449d9df6a7376b9c8
rdmccray <dennis@themccrays.net>
2024-05-21 21:32:38 -0500

    cleanup, more lsp config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -14,7 +14,7 @@ return {
     },
     config = function()
         local cmp = require("cmp")
-        local luasnip = require("luasnip")
+        -- local luasnip = require("luasnip")
         local lspkind = require("lspkind")
 
 
@@ -33,8 +33,8 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-k>"] = cmp.mapping.select_next_item(),
-                ["<C-j>"] = cmp.mapping.select_prev_item(),
+                ["<C-j>"] = cmp.mapping.select_next_item(),
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
@@ -50,18 +50,11 @@ return {
             formatting = {
                 format = lspkind.cmp_format({
                     mode = 'symbol_text', -- show only symbol annotations
-                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                                    -- can also be a function to dynamically calculate max width such as 
-                                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
                     show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-                    before = function (entry, vim_item)
-                        -- The function below will be called before any 
-                        -- actual modifications from lspkind so that you 
-                        -- can provide more controls on popup customization
-                        -- (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-                        return vim_item
-                    end,
                 })
             }
         })
@@ -69,22 +62,22 @@ return {
         cmp.setup.cmdline('/', {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
-              { name = 'buffer' }
+                { name = 'buffer' }
             }
         })
-          -- `:` cmdline setup.
+        -- `:` cmdline setup.
         cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
                 { name = 'path' }
             }, {
-                    {
-                        name = 'cmdline',
-                        option = {
-                            ignore_cmds = { 'Man', '!' },
-                            treat_trailing_slash = true,
-                        }
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' },
+                        treat_trailing_slash = true,
                     }
+                }
             })
         })
     end
