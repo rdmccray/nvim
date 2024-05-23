23fbd652ac262e2390d952233ab015df4507a45d
rdmccray <dennis@themccrays.net>
2024-05-23 18:16:08 -0500

    cleaning up LSP config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,13 @@
 return {
     "williamboman/mason.nvim",
-    event = "BufReadPre",
+    dependencies = {
+        "williamboman/mason-lspconfig",
+        "WhoIsSethDaniel/mason-tool-installer",
+    },
     config = function()
+        -- variable for advanced  setup capabilities
+        -- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
         require("mason").setup({
             ui = {
                 icons = {
@@ -11,5 +17,65 @@ return {
                 },
             },
         })
-    end
+
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+            },
+            automatic_installation = true,
+            -- placeholder for advanced setup functionality of mason plugins below
+            -- handlers = {
+            --     function(server_name)
+            --         require("lspconfig")[server_name].setup({
+            --             capabilities = lsp_capabilities,
+            --         })
+            --     end,
+            -- },
+        })
+
+        require("mason-tool-installer").setup({
+
+            -- a list of all tools you want to ensure are installed upon
+            -- start
+            ensure_installed = {
+                "stylua",
+                "prettier",
+            },
+
+            -- if set to true this will check each tool for updates. If updates
+            -- are available the tool will be updated. This setting does not
+            -- affect :MasonToolsUpdate or :MasonToolsInstall.
+            -- Default: false
+            auto_update = true,
+
+            -- automatically install / update on startup. If set to false nothing
+            -- will happen on startup. You can use :MasonToolsInstall or
+            -- :MasonToolsUpdate to install tools and check for updates.
+            -- Default: true
+            run_on_start = true,
+
+            -- set a delay (in ms) before the installation starts. This is only
+            -- effective if run_on_start is set to true.
+            -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+            -- Default: 0
+            start_delay = 3000, -- 3 second delay
+
+            -- Only attempt to install if 'debounce_hours' number of hours has
+            -- elapsed since the last time Neovim was started. This stores a
+            -- timestamp in a file named stdpath('data')/mason-tool-installer-debounce.
+            -- This is only relevant when you are using 'run_on_start'. It has no
+            -- effect when running manually via ':MasonToolsInstall' etc....
+            -- Default: nil
+            debounce_hours = nil, -- at least 5 hours between attempts to install/update
+
+            -- Disable integration with other Mason plugins. This removes
+            -- the ability to to use the alternative names of packages provided
+            -- by these plugins but disables them from immediately becoming loaded
+            integrations = {
+                ["mason-lspconfig"] = true,
+                ["mason-null-ls"] = true,
+                ["mason-nvim-dap"] = true,
+            },
+        })
+    end,
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,5 @@
 return {
     "neovim/nvim-lspconfig",
-    -- lazy = false,
     dependencies = {
         "folke/neoconf.nvim",
         "folke/neodev.nvim",
@@ -14,84 +13,81 @@ return {
 
     config = function()
         -- add diagnositc signs to the number column
+
         local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         end
 
-        -- setting up for configuring language servers
-        require("neodev").setup({})
-        require("neoconf").setup({})
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-        local lspconfig = require("lspconfig")
+        -- keymaps to use in the buffer
 
-        local on_attach = function(_, _)
-            vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-            vim.keymap.set("n", "<leader>gg", "<cmd>lua vim.lsp.buf.hover()<CR>")
-            vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-            vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-            vim.keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-            vim.keymap.set("n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-            vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-            vim.keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-            vim.keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.rename()<CR>")
-            vim.keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
-            vim.keymap.set("n", "<leader>ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-            vim.keymap.set("n", "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
-            vim.keymap.set("n", "<leader>gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
-            vim.keymap.set("n", "<leader>gn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-            vim.keymap.set("n", "<leader>tr", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
-            vim.keymap.set("i", "<C-Space>", "<cmd>lua vim.lsp.buf.completion()<CR>")
+        local on_attach = function(_, bufnr)
+            vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gg", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gn", "<cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>tr", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { buffer = bufnr })
+            vim.keymap.set("i", "<C-Space>", "<cmd>lua vim.lsp.buf.completion()<CR>", { buffer = bufnr })
         end
 
-        -- begin configuring language servers
+        -- setting up for configuring language servers
+
+        require("neodev").setup({})
+        require("neoconf").setup({})
+        local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local lspconfig = require("lspconfig")
+
+        -- **************************************
+        -- * begin configuring language servers *
+        -- **************************************
 
         -- lua-language-server
+
         lspconfig.lua_ls.setup({
-            capabilities = capabilities,
+            capabilities = lsp_capabilities,
             on_attach = on_attach,
-
-            on_init = function(client)
-                local path = client.workspace_folders[1].name
-                if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-                    return
-                end
-
-                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using
-                        -- (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                    },
-                    -- Make the server aware of Neovim runtime files
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME,
-                            -- Depending on the usage, you might want to add additional paths here.
-                            -- "${3rd}/luv/library"
-                            -- "${3rd}/busted/library",
-                        },
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                        -- library = vim.api.nvim_get_runtime_file("", true)
-                    },
-                })
-            end,
             settings = { -- custom settings for lua
                 Lua = {
+                    completion = {
+                        callSnippet = "Replace",
+                        enable = true,
+                    },
                     -- make the language server recognize "vim" global
                     diagnostics = {
+                        disable = {
+                            "missing-fields",
+                        },
                         globals = { "vim" },
+                    },
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        ignoreDir = { ".git" },
+                        library = {
+                            vim.env.VIMRUNTIME,
+                        },
                     },
                 },
             },
         })
 
         -- typescript language server
+
         lspconfig.tsserver.setup({
-            capabilities = capabilities,
+            capabilities = lsp_capabilities,
             on_attach = on_attach,
             init_options = {
                 hostInfo = "Neovim",
@@ -102,11 +98,12 @@ return {
         })
 
         -- json language server
+
         lspconfig.jsonls.setup({
-            capabilities = capabilities,
+            capabilities = lsp_capabilities,
             on_attach = on_attach,
             init_options = {
-                capabilities.textDocument.completion.completionItem.snippetSupport == true,
+                lsp_capabilities.textDocument.completion.completionItem.snippetSupport == true,
             },
         })
     end,
