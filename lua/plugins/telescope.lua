2d2fb17be68e5dc22990123f635ae82a04e4ab81
rdmccray <dennis@themccrays.net>
2024-06-05 16:01:04 -0500

    added help to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,56 +1,64 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	tag = "0.1.6",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"nvim-telescope/telescope-ui-select.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
-		local builtin = require("telescope.builtin")
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    tag = "0.1.6",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "nvim-telescope/telescope-ui-select.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
+        local builtin = require("telescope.builtin")
 
-		telescope.setup({
-			defaults = {
-				file_ignore_patterns = {
-					"node_modules",
-				},
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["Q"] = actions.close,
-					},
-				},
-			},
-			pickers = {
-				find_files = {
-					mappings = {},
-					hidden = true,
-				},
-			},
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({
-						-- even more opts
-					}),
-				},
-			},
-		})
+        telescope.setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".git",
+                },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next, -- move to next result
+                        ["Q"] = actions.close,
+                    },
+                },
+                -- previewer = false,
+            },
+            pickers = {
+                find_files = {
+                    mappings = {},
+                    previewer = false,
+                    hidden = true,
+                    theme = "dropdown",
+                },
+                help_tags = {
+                    treesitter = false,
+                },
+            },
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({
+                        -- even more opts
+                    }),
+                },
+            },
+        })
 
-		telescope.load_extension("fzf")
-		telescope.load_extension("ui-select")
+        telescope.load_extension("fzf")
+        telescope.load_extension("ui-select")
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+        -- set keymaps
+        local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>ft", builtin.buffers, { desc = "Find TODOs" })
-		keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find string in cwd" })
-	end,
+        keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>ft", builtin.buffers, { desc = "Find TODOs" })
+        keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+        keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search the help" })
+    end,
 }
